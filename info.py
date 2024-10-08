ad25264649f526e842355b5ac6e6b72e5db0b6bb
Panksour <96158441+Panksour@users.noreply.github.com>
2024-10-08 20:45:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,9 +51,9 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
+DATABASE_URI = environ.get('mongodb+srv://pnk:<db_pnk>@cluster0.lfzcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', "")
+DATABASE_NAME = environ.get('cluster0', "techvjautobot")
+COLLECTION_NAME = environ.get('cluster0', 'techvjcollection')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', false)) # Set Ture Or False
